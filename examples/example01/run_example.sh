5c432c8bdee1c07c45f88f52ebb6a0e77955ae4d
sergioh <sergioh.pessoal@gmail.com>
2026-04-17 14:54:58 -0300

    small changes

--- examples/example01/run_example.sh
+++ examples/example01/run_example.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 export MBUFR_TABLES=../../bufrtables
 
-#--------------------------------------------
-# Encode daycli message im BUFR from text file
-#----------------------------------------------
-../../bin/daycli_encoder1 -i ./DAYCLI_82191.txt -o ./DAYCLI_sample_82191.bufr
-../../bin/daycli_encoder1 -i ./DAYCLI_MF_0-20000-0-71805_202204.txt -o ./DAYCLI_MF_0-20000-0-71805_202204.bufr
+#-------------------------------------------------------
+# Encode daycli message in BUFR from text file (Format1)
+#--------------------------------------------------------
+../../bin/daycli_encoder1 -i ./DAYCLI_82191_f1.txt -o ./DAYCLI_sample_82191.bufr
+../../bin/daycli_encoder1 -i ./DAYCLI_MF_0-20000-0-71805_202204_f1.txt -o ./DAYCLI_MF_0-20000-0-71805_202204.bufr
+#----------------------------------------------------------------------------
+# Decode DAYCLI message from BUFR to text file using daycli_decoder (format2)
+#---------------------------------------------------------------------------
+../../bin/daycli_decoder -i ./DAYCLI_sample_82191.bufr -o ./DAYCLI_sample_82191.decoded.f2.txt
+../../bin/daycli_decoder -i ./DAYCLI_MF_0-20000-0-71805_202204.bufr -o ./DAYCLI_MF_0-20000-0-71805_202204.decoded_f2.txt
+
 #-------------------------------------------------------------
-# Decode DAYCLI message from BUFR to text file (using bufrdump)
+# Decode DAYCLI message from BUFR to text file using BUFRDUMP
 #--------------------------------------------------------------
-../../bin/daycli_decoder -i ./DAYCLI_sample_82191.bufr -o ./DAYCLI_sample_82191.format2.txt
-../../bin/daycli_decoder -i ./DAYCLI_MF_0-20000-0-71805_202204.bufr -o ./DAYCLI_MF_0-20000-0-71805_202204.formato2.txt
-../../bin/bufrdump -i ./DAYCLI_sample_82191.bufr -o DAYCLI_sample_82191.bufr.decoded.txt
-../../bin/bufrdump -i ./DAYCLI_MF_0-20000-0-71805_202204.bufr -o ./DAYCLI_MF_0-20000-0-71805_202204.bufr.decoded.txt
+../../bin/bufrdump -i ./DAYCLI_sample_82191.bufr -o DAYCLI_sample_82191.decoded.txt
+../../bin/bufrdump -i ./DAYCLI_MF_0-20000-0-71805_202204.bufr -o ./DAYCLI_MF_0-20000-0-71805_202204.decoded.txt
 
